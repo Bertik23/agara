2525248521ab9f62f12bc4bcfcbf3e208f460e7d
Bertik23 <bertikxxiii@gmail.com>
2024-07-06 14:43:06 +0200

    idk

--- src/parser.rs
+++ src/parser.rs
@@ -2,179 +2,236 @@ use core::panic;
 
 extern crate lazy_static;
 
-
 use lazy_static::lazy_static;
 
-use crate::tokenizer::{Token};
+use crate::tokenizer::Token;
 use std::{collections::HashMap, mem};
 
-lazy_static!{
-    static ref BINOP_PRECEDENSE: HashMap<&'static str, i32> = HashMap::from(
-        [
-            (" ", -10),
-            ("+", 10),
-            ("-", 10),
-            ("*", 20),
-            ("%", 20),
-            ("/", 20),
-            ("**", 30),
-            ("=",1)
-        ]
-    );
+lazy_static! {
+    static ref BINOP_PRECEDENSE: HashMap<&'static str, i32> = HashMap::from([
+        (" ", -10),
+        ("+", 10),
+        ("-", 10),
+        ("*", 20),
+        ("%", 20),
+        ("/", 20),
+        ("**", 30),
+        ("=", 1)
+    ]);
 }
 
 #[derive(Debug, Clone)]
-pub enum ASTTree{
+pub enum ASTTree {
     Number(f64),
     Variable(String),
     UnaryOp(String, Box<ASTTree>),
     BinaryOp(String, Box<ASTTree>, Box<ASTTree>),
     Call(String, Vec<ASTTree>),
     Function(String, Vec<ASTTree>, Vec<ASTTree>),
-    String(String)
+    String(String),
 }
 
-fn parse_number(tokens: &mut Vec<Token>) -> ASTTree{
-    ASTTree::Number(if let Token::Numb(n, _) = tokens.pop().unwrap(){n} else {panic!("WTF")})
+fn parse_number(tokens: &mut Vec<Token>) -> ASTTree {
+    ASTTree::Number(if let Token::Numb(n, _) = tokens.pop().unwrap() {
+        n
+    } else {
+        panic!("WTF")
+    })
 }
 
-fn parse_bin_op_rhs(tokens: &mut Vec<Token>, lhs_: ASTTree, min_token_precedense: i32) -> ASTTree{
+fn parse_bin_op_rhs(tokens: &mut Vec<Token>, lhs_: ASTTree, min_token_precedense: i32) -> ASTTree {
     let mut lhs = lhs_.clone();
-    loop{
+    loop {
         let tok_precedense;
         {
             let cur_tok = tokens.last().unwrap();
-            tok_precedense = BINOP_PRECEDENSE.get(
-                if let Token::Operator(op, _) = cur_tok { op.as_str() }
-                else {return lhs}
-            ).unwrap_or(&-1);
+            tok_precedense = BINOP_PRECEDENSE
+                .get(if let Token::Operator(op, _) = cur_tok {
+                    op.as_str()
+                } else {
+                    return lhs;
+                })
+                .unwrap_or(&-1);
         }
-        if *tok_precedense < min_token_precedense{
+        if *tok_precedense < min_token_precedense {
             return lhs;
         }
         let operator = tokens.pop().unwrap();
         let mut rhs = parse_primary(tokens);
-        let next_tok_precedense = BINOP_PRECEDENSE.get(
-            if let Token::Operator(op, _) = tokens.last().unwrap() { op.as_str() }
-            else {" "}
-        ).unwrap_or(&-1);
-        if tok_precedense < next_tok_precedense{
+        let next_tok_precedense = BINOP_PRECEDENSE
+            .get(if let Token::Operator(op, _) = tokens.last().unwrap() {
+                op.as_str()
+            } else {
+                " "
+            })
+            .unwrap_or(&-1);
+        if tok_precedense < next_tok_precedense {
             rhs = parse_bin_op_rhs(tokens, rhs, tok_precedense + 1);
         }
         lhs = ASTTree::BinaryOp(
-            if let Token::Operator(op, _) = operator { op }
-            else {panic!("Imposible")},
+            if let Token::Operator(op, _) = operator {
+                op
+            } else {
+                panic!("Imposible")
+            },
             Box::new(lhs),
-            Box::new(rhs)
+            Box::new(rhs),
         );
     }
 }
 
-fn parse_paren(tokens: &mut Vec<Token>) -> ASTTree{
+fn parse_paren(tokens: &mut Vec<Token>) -> ASTTree {
     tokens.pop();
     let lhs = parse_expression(tokens);
-    if tokens.pop().unwrap() != Token::RParen(0){
+    if tokens.pop().unwrap() != Token::RParen(0) {
         panic!("Expected ')'");
     }
 
     return lhs;
 }
 
-fn parse_unary(tokens: &mut Vec<Token>) -> ASTTree{
-    let op = if let Token::Operator(op, _) = tokens.pop().unwrap() { op } else { panic!("Imposible") };
+fn parse_unary(tokens: &mut Vec<Token>) -> ASTTree {
+    let op = if let Token::Operator(op, _) = tokens.pop().unwrap() {
+        op
+    } else {
+        panic!("Imposible")
+    };
     return ASTTree::UnaryOp(op, Box::new(parse_primary(tokens)));
 }
 
-fn parse_init(tokens: &mut Vec<Token>) -> ASTTree{
+fn parse_init(tokens: &mut Vec<Token>) -> ASTTree {
     let lhs;
     match tokens.pop().unwrap() {
-        Token::Ident(id, _) => {lhs = ASTTree::Variable(id);}
-        _ => panic!("Expected identifier.")
+        Token::Ident(id, _) => {
+            lhs = ASTTree::Variable(id);
+        }
+        _ => panic!("Expected identifier."),
     }
 
-    return parse_bin_op_rhs(tokens, lhs, 0)
+    return parse_bin_op_rhs(tokens, lhs, 0);
 }
 
-fn parse_function_def(tokens: &mut Vec<Token>) -> ASTTree{
+fn parse_function_def(tokens: &mut Vec<Token>) -> ASTTree {
     let mut params: Vec<ASTTree> = vec![];
     let Token::Ident(name, _) = tokens.pop().unwrap()
     else {panic!("Expected function name")};
-    if let Token::LParen(_) = tokens.pop().unwrap() {} else {panic!("Expected `(`")}
-    while let Token::Ident(name, _) = tokens.pop().unwrap(){
+    if let Token::LParen(_) = tokens.pop().unwrap() {
+    } else {
+        panic!("Expected `(`")
+    }
+    while let Token::Ident(name, _) = tokens.pop().unwrap() {
         params.push(ASTTree::Variable(name))
     }
 
-    if let Token::StartBlock(_) = tokens.pop().unwrap() {} else {panic!("Expected code block")}
+    if let Token::StartBlock(_) = tokens.pop().unwrap() {
+    } else {
+        panic!("Expected code block")
+    }
     dbg!(&tokens);
     let next = parse(tokens);
     dbg!(&next);
     return ASTTree::Function(name, params, next);
 }
 
-fn parse_call(tokens: &mut Vec<Token>) -> Vec<ASTTree>{
+fn parse_call(tokens: &mut Vec<Token>) -> Vec<ASTTree> {
     let mut params = vec![];
     tokens.pop();
-    while if let Token::RParen(_) = tokens.last().unwrap() {false} else {true} {
+    while if let Token::RParen(_) = tokens.last().unwrap() {
+        false
+    } else {
+        true
+    } {
         params.push(parse_primary(tokens));
     }
     tokens.pop();
     return params;
 }
 
-fn parse_ident(tokens: &mut Vec<Token>) -> ASTTree{
-    match tokens.pop().unwrap(){
+fn parse_ident(tokens: &mut Vec<Token>) -> ASTTree {
+    match tokens.pop().unwrap() {
         Token::Ident(id, _) => {
             match id.as_str() {
                 "let" => return parse_init(tokens),
                 "fun" => return parse_function_def(tokens),
                 _ => {
-                    if let Token::LParen(_) = tokens.last().unwrap(){
-                        return ASTTree::Call(id, parse_call(tokens))
+                    if let Token::LParen(_) = tokens.last().unwrap() {
+                        return ASTTree::Call(id, parse_call(tokens));
                     }
-                    return ASTTree::Variable(id)
-                }
-                // _ => panic!("Unknown indentifier `{}`", id)
+                    return ASTTree::Variable(id);
+                } // _ => panic!("Unknown indentifier `{}`", id)
             }
         }
-        _ => panic!("Can't happen.")
+        _ => panic!("Can't happen."),
     }
 }
 
-fn parse_string(tokens: &mut Vec<Token>) -> ASTTree{
+fn parse_string(tokens: &mut Vec<Token>) -> ASTTree {
     match tokens.pop().unwrap() {
-        Token::String(string, _) => {return ASTTree::String(string)}
-        _ => panic!("Cant happen.")
+        Token::String(string, _) => return ASTTree::String(string),
+        _ => panic!("Cant happen."),
     }
 }
 
-fn parse_primary(tokens: &mut Vec<Token>) -> ASTTree{
+fn parse_primary(tokens: &mut Vec<Token>) -> ASTTree {
     let lhs;
-    match tokens.last().unwrap(){
-        Token::Numb(_,_) => {lhs = parse_number(tokens);}
-        Token::LParen(_) => {lhs = parse_paren(tokens);}
-        Token::Operator(_,_) => {lhs = parse_unary(tokens);}
-        Token::Ident(_, _) => {lhs = parse_ident(tokens);}
-        Token::String(_, _) => {lhs = parse_string(tokens);}
-        default => {panic!("Parser error: Unexpected token. {:?}.\nRest of tokens: {:?}", default, tokens)}
+    match tokens.last().unwrap() {
+        Token::Numb(_, _) => {
+            lhs = parse_number(tokens);
+        }
+        Token::LParen(_) => {
+            lhs = parse_paren(tokens);
+        }
+        Token::Operator(_, _) => {
+            lhs = parse_unary(tokens);
+        }
+        Token::Ident(_, _) => {
+            lhs = parse_ident(tokens);
+        }
+        Token::String(_, _) => {
+            lhs = parse_string(tokens);
+        }
+        Token::Unknown => {
+            panic!("Unknown token.");
+        }
+        Token::EOF(_) => {
+            lhs = ASTTree::Number(0.0);
+            // tokens.pop();
+            // lhs = parse_primary(tokens);
+        }
+        default => {
+            panic!(
+                "Parser error: Unexpected token. {:?}.\nRest of tokens: {:?}",
+                default, tokens
+            )
+        }
     }
     return lhs;
 }
 
-fn parse_expression(tokens: &mut Vec<Token>) -> ASTTree{
+fn parse_expression(tokens: &mut Vec<Token>) -> ASTTree {
     let lhs = parse_primary(tokens);
 
-    return parse_bin_op_rhs(tokens, lhs, 0)
+    return parse_bin_op_rhs(tokens, lhs, 0);
 }
 
-pub fn parse(tokens: &mut Vec<Token>) -> Vec<ASTTree>{
+pub fn parse(tokens: &mut Vec<Token>) -> Vec<ASTTree> {
     let mut ast: Vec<ASTTree> = vec![];
     // tokens.reverse();
     loop {
         match tokens.last().unwrap() {
-            Token::EOF(_) => {return ast}
-            Token::Delim(_) => {tokens.pop(); continue;}
-            Token::EndBlock(_) => {dbg!("Hello", tokens.pop(), &ast); return ast}
-            _ => {ast.push(parse_expression(tokens));}
+            Token::EOF(_) => return ast,
+            Token::Delim(_) => {
+                tokens.pop();
+                continue;
+            }
+            Token::EndBlock(_) => {
+                dbg!("Hello", tokens.pop(), &ast);
+                return ast;
+            }
+            _ => {
+                ast.push(parse_expression(tokens));
+            }
         }
     }
 }
+
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -2,38 +2,31 @@ extern crate regex_macro;
 use regex_macro::regex;
 use std::mem;
 
-pub use self::Token::{
-    Ident,
-    Numb,
-    Delim,
-    Operator,
-    EOF,
-    StartBlock,
-    EndBlock,
-};
+pub use self::Token::{Delim, EndBlock, Ident, Numb, Operator, StartBlock, EOF};
 
 #[derive(Debug)]
 pub enum Token {
-    Ident(String,usize),
-    Numb(f64,usize),
-    Operator(String,usize),
+    Ident(String, usize),
+    Numb(f64, usize),
+    Operator(String, usize),
     Delim(usize),
     LParen(usize),
     RParen(usize),
     EOF(usize),
     String(String, usize),
     StartBlock(usize),
-    EndBlock(usize)
+    EndBlock(usize),
+    Unknown,
 }
 
 impl PartialEq for Token {
     fn ne(&self, other: &Self) -> bool {
-        if mem::discriminant(self) == mem::discriminant(other){
+        if mem::discriminant(self) == mem::discriminant(other) {
             match (self, other) {
-                (Ident(name, _), Ident(oname,_)) => {name == oname}
-                (Operator(name, _), Operator(oname,_)) => {name == oname}
-                (Numb(name, _), Numb(oname,_)) => {name == oname}
-                _ => false
+                (Ident(name, _), Ident(oname, _)) => name == oname,
+                (Operator(name, _), Operator(oname, _)) => name == oname,
+                (Numb(name, _), Numb(oname, _)) => name == oname,
+                _ => false,
             }
         } else {
             false
@@ -44,7 +37,7 @@ impl PartialEq for Token {
     }
 }
 
-pub fn tokenize(input: &str) -> Vec<Token>{
+pub fn tokenize(input: &str) -> Vec<Token> {
     let r = regex!(concat!(
         r"(?P<ident>\p{Alphabetic}\w*)|",
         r"(?P<num>\d+\.?\d*)|",
@@ -54,37 +47,43 @@ pub fn tokenize(input: &str) -> Vec<Token>{
         r"(?P<rpar>\))|",
         r"(?P<sbl>\{)|",
         r"(?P<ebl>\})|",
-        r"(?P<op>[+=%\-*<>!/]+)"
+        r"(?P<op>[+=%\-*<>!/]+)",
+        r"(?P<else>.*)"
     ));
     let mut out: Vec<Token> = vec![];
-    for cap in r.captures_iter(input){
-        let t: Token = if cap.name("ident").is_some(){
+    for cap in r.captures_iter(input) {
+        let t: Token = if cap.name("ident").is_some() {
             Token::Ident(
                 cap.name("ident").unwrap().as_str().to_string(),
                 cap.name("ident").unwrap().start(),
             )
-        } else if cap.name("num").is_some(){
-            match cap.name("num").unwrap().as_str().parse(){
+        } else if cap.name("num").is_some() {
+            match cap.name("num").unwrap().as_str().parse() {
                 Ok(number) => Token::Numb(number, cap.name("num").unwrap().start()),
-                Err(_) => panic!("Lexer failed to parse number")
+                Err(_) => panic!("Lexer failed to parse number"),
             }
-        } else if cap.name("delim").is_some(){
+        } else if cap.name("delim").is_some() {
             Token::Delim(cap.name("delim").unwrap().start())
-        } else if cap.name("op").is_some(){
+        } else if cap.name("op").is_some() {
             Token::Operator(
                 cap.name("op").unwrap().as_str().to_string(),
-                cap.name("op").unwrap().start()
+                cap.name("op").unwrap().start(),
             )
-        } else if cap.name("lpar").is_some(){
+        } else if cap.name("lpar").is_some() {
             Token::LParen(cap.name("lpar").unwrap().start())
-        } else if cap.name("rpar").is_some(){
+        } else if cap.name("rpar").is_some() {
             Token::RParen(cap.name("rpar").unwrap().start())
-        } else if cap.name("sbl").is_some(){
+        } else if cap.name("sbl").is_some() {
             Token::StartBlock(cap.name("sbl").unwrap().start())
-        } else if cap.name("ebl").is_some(){
+        } else if cap.name("ebl").is_some() {
             Token::EndBlock(cap.name("ebl").unwrap().start())
-        } else if cap.name("str").is_some(){
-            Token::String(cap.name("str").unwrap().as_str().to_string(), cap.name("str").unwrap().start())
+        } else if cap.name("str").is_some() {
+            Token::String(
+                cap.name("str").unwrap().as_str().to_string(),
+                cap.name("str").unwrap().start(),
+            )
+        } else if cap.name("else").is_some() {
+            Token::Unknown
         } else {
             panic!("Wtf")
         };
@@ -93,5 +92,3 @@ pub fn tokenize(input: &str) -> Vec<Token>{
     out.push(EOF(input.len()));
     out
 }
-
-
